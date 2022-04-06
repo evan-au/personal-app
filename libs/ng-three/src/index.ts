3259d9aa4ef33b33f6847c569f28e0acd8ea3fef
Evan Pierre Louis <evan.dev@yandex.com>
2022-04-06 02:58:33 +0200

    refactor

--- libs/ng-three/src/index.ts
+++ libs/ng-three/src/index.ts
@@ -1,4 +1,11 @@
 export * from './lib/ng-three.module';
+export * from './lib/ui/ui.module';
+
+// Components
+export * from './lib/ui/particles/particles.component';
+export * from './lib/ui/slider/slider.component';
+export * from './lib/ui/snowflakes-canvas/snowflakes-canvas.component';
+export * from './lib/ui/terrain-canvas/terrain-canvas.component';
 
 // Directives
 export * from './lib/utils/directives/breakpoint-check-mobile.directive';
