145e1aca3398b160363ca1acb5406280da9e28a0
Evan Pierre Louis <evan.dev@yandex.com>
2022-04-07 15:00:47 +0200

    add digital-flyer lib to app

--- apps/personal-app/src/app/app.module.ts
+++ apps/personal-app/src/app/app.module.ts
@@ -4,10 +4,23 @@ import { NgThreeModule } from '@personal-app/ng-three';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@personal-app/digital-flyer').then((m) => m.DigitalFlyerModule),
+  },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgThreeModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    NgThreeModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
