c819905d202412ceb9a5bd603aecd87ce7706639
Evan Pierre Louis <evan.dev@yandex.com>
2022-04-07 15:16:20 +0200

    app: remove ngthree module

--- apps/personal-app/src/app/app.module.ts
+++ apps/personal-app/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgThreeModule } from '@personal-app/ng-three';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -15,12 +13,7 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    NgThreeModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
