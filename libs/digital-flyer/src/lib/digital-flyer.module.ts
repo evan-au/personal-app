c9d46a63aceb976e03ee15f1293a37abf372e3e7
Evan Pierre Louis <evan.dev@yandex.com>
2022-04-07 15:01:40 +0200

    fix routes

--- libs/digital-flyer/src/lib/digital-flyer.module.ts
+++ libs/digital-flyer/src/lib/digital-flyer.module.ts
@@ -7,24 +7,16 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./features/landing/landing.module').then((m) => m.LandingModule),
-    children: [
-      {
-        path: 'sign-in',
-        loadChildren: () =>
-          import('./features/sign-in/sign-in.module').then(
-            (m) => m.SignInModule
-          ),
-      },
-      {
-        path: 'editor',
-        loadChildren: () =>
-          import('./features/editor/editor.module').then((m) => m.EditorModule),
-      },
-      // {
-      //   path: 'sign-up',
-      //   loadChildren: () => import('./features/landing/landing.module').then(m=>m.LandingModule),
-      // },
-    ],
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/sign-in/sign-in.module').then((m) => m.SignInModule),
+  },
+  {
+    path: 'editor',
+    loadChildren: () =>
+      import('./features/editor/editor.module').then((m) => m.EditorModule),
   },
 ];
 @NgModule({
